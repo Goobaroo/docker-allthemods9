6bde69a33dc9335093118a58d04d29e1da444ceb
Jason McLeod <jason@mcleods.me>
2023-11-08 20:08:07 -0500

    Update to version 0.2.14

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# syntax=docker/dockerfile:1
+
+FROM openjdk:17-buster
+
+LABEL version="0.2.14"
+
+RUN apt-get update && apt-get install -y curl unzip && \
+ adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
+
+COPY launch.sh /launch.sh
+RUN chmod +x /launch.sh
+
+USER minecraft
+
+VOLUME /data
+WORKDIR /data
+
+EXPOSE 25565/tcp
+
+CMD ["/launch.sh"]
---
+++ launch.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -x
+
+FORGE_VERSION=1.20.1-47.1.3
+cd /data
+
+if ! [[ "$EULA" = "false" ]]; then
+	echo "eula=true" > eula.txt
+else
+	echo "You must accept the EULA to install."
+	exit 99
+fi
+
+if ! [[ -f 'Server-Files-0.2.14.zip' ]]; then
+	rm -fr defaultconfigs config kubejs libraries mods Simple.zip forge*
+	curl -Lo 'Server-Files-0.2.14.zip' 'https://edge.forgecdn.net/files/4846/798/Server-Files-0.2.14.zip' || exit 9
+	unzip -u -o 'Server-Files-0.2.14.zip' -d /data
+	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
+	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
+		cd "${DIR_TEST}"
+		mv -f * /data
+		cd /data
+		rm -fr "$DIR_TEST"
+	fi
+	curl -Lo forge-${FORGE_VERSION}-installer.jar http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGE_VERSION/forge-$FORGE_VERSION-installer.jar
+	java -jar forge-${FORGE_VERSION}-installer.jar --installServer
+fi
+
+if [[ -n "$JVM_OPTS" ]]; then
+	sed -i '/-Xm[s,x]/d' user_jvm_args.txt
+	for j in ${JVM_OPTS}; do sed -i '$a\'$j'' user_jvm_args.txt; done
+fi
+if [[ -n "$MOTD" ]]; then
+    sed -i "s/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' > ops.txt
+fi
+if [[ -n "$ALLOWLIST" ]]; then
+    echo $ALLOWLIST | awk -v RS=, '{print}' > white-list.txt
+fi
+
+sed -i 's/server-port.*/server-port=25565/g' server.properties
+chmod 755 run.sh
+
+./run.sh
