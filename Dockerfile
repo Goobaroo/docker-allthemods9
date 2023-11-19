ddf8fce01976967eb648792a132ed24592bb69e6
Jason McLeod <jason@mcleods.me>
2023-11-19 09:11:11 -0500

    Update to version 0.0.65

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM openjdk:17-buster
 
-LABEL version="0.1.0"
+LABEL version="0.0.65"
 
 RUN apt-get update && apt-get install -y curl unzip && \
  adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
--- launch.sh
+++ launch.sh
@@ -12,10 +12,10 @@ else
 	exit 99
 fi
 
-if ! [[ -f 'Server-Files-0.1.0.zip' ]]; then
+if ! [[ -f 'Server-Files-0.0.65.zip' ]]; then
 	rm -fr defaultconfigs config kubejs libraries mods Simple.zip forge*
-	curl -Lo 'Server-Files-0.1.0.zip' 'https://edge.forgecdn.net/files/4752/160/Server-Files-0.1.0.zip' || exit 9
-	unzip -u -o 'Server-Files-0.1.0.zip' -d /data
+	curl -Lo 'Server-Files-0.0.65.zip' 'https://edge.forgecdn.net/files/4742/441/Server-Files-0.0.65.zip' || exit 9
+	unzip -u -o 'Server-Files-0.0.65.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
 		cd "${DIR_TEST}"
