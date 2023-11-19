a6270c5d6590a7cc54319b4054064248e1fdd2d6
Jason McLeod <jason@mcleods.me>
2023-11-19 08:35:54 -0500

    Update to version 0.2.18

--- launch.sh
+++ launch.sh
@@ -12,10 +12,10 @@ else
 	exit 99
 fi
 
-if ! [[ -f 'Server-Files-0.2.18b.zip' ]]; then
+if ! [[ -f 'Server-Files-0.2.18.zip' ]]; then
 	rm -fr defaultconfigs config kubejs libraries mods Simple.zip forge*
-	curl -Lo 'Server-Files-0.2.18b.zip' 'https://edge.forgecdn.net/files/4861/3/Server-Files-0.2.18b.zip' || exit 9
-	unzip -u -o 'Server-Files-0.2.18b.zip' -d /data
+	curl -Lo 'Server-Files-0.2.18.zip' 'https://edge.forgecdn.net/files/4860/856/Server-Files-0.2.18.zip' || exit 9
+	unzip -u -o 'Server-Files-0.2.18.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
 		cd "${DIR_TEST}"
