7c38e0326d9d5008db247ecb03506d76f992a15d
Jason McLeod <jason@mcleods.me>
2023-12-31 13:51:02 -0500

    Update to version 0.2.34

--- launch.sh
+++ launch.sh
@@ -13,7 +13,7 @@ else
 fi
 
 if ! [[ -f 'Server-Files-0.2.34.zip' ]]; then
-	rm -fr defaultconfigs config kubejs libraries mods Simple.zip forge*
+	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
 	curl -Lo 'Server-Files-0.2.34.zip' 'https://edge.forgecdn.net/files/4985/960/Server-Files-0.2.34.zip' || exit 9
 	unzip -u -o 'Server-Files-0.2.34.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
