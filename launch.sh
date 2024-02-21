55e4c85309bc6d0c2c163631ebd0cbc896502e7c
Jason McLeod <jason@mcleods.me>
2024-02-21 08:57:58 -0500

    Update to version 0.2.50

--- launch.sh
+++ launch.sh
@@ -12,10 +12,10 @@ else
 	exit 99
 fi
 
-if ! [[ -f 'Server-Files-0.2.49.zip' ]]; then
+if ! [[ -f 'Server-Files-0.2.50.zip' ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo 'Server-Files-0.2.49.zip' 'https://edge.forgecdn.net/files/5122/509/Server-Files-0.2.49.zip' || exit 9
-	unzip -u -o 'Server-Files-0.2.49.zip' -d /data
+	curl -Lo 'Server-Files-0.2.50.zip' 'https://edge.forgecdn.net/files/5124/810/Server-Files-0.2.50.zip' || exit 9
+	unzip -u -o 'Server-Files-0.2.50.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
 		cd "${DIR_TEST}"
