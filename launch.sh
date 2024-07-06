461b624bab4241ceeade37d0deaab9b025a46746
Cobb <32686451+Cobbert@users.noreply.github.com>
2024-07-06 11:34:29 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -24,7 +24,7 @@ if ! [[ -f 'Server-Files-0.2.61.zip' ]]; then
 		rm -fr "$DIR_TEST"
 	fi
 	curl -Lo forge-${FORGE_VERSION}-installer.jar https://mohistmc.com/api/v2/projects/mohist/1.20.1/builds/741/mohist-1.20.1-741-server.jar
-	java -jar forge-${FORGE_VERSION}-installer.jar --installServer
+	java -jar -Xmx4G downloaded-jar-file.jar
 fi
 
 if [[ -n "$JVM_OPTS" ]]; then
