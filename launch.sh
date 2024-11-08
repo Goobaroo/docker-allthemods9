9556affe338e101e828c008a59dc6ce24e4a96f3
Jason McLeod <jason@mcleods.me>
2024-11-08 08:04:03 -0500

    Update to version 0.3.3

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-FORGE_VERSION=1.20.1-47.3.7
+FORGE_VERSION=1.20.1-47.3.11
 cd /data
 
 if ! [[ "$EULA" = "false" ]]; then
@@ -12,10 +12,10 @@ else
 	exit 99
 fi
 
-if ! [[ -f 'Server-Files-0.3.2.zip' ]]; then
+if ! [[ -f 'Server-Files-0.3.3.zip' ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo 'Server-Files-0.3.2.zip' 'https://edge.forgecdn.net/files/5682/556/Server-Files-0.3.2.zip' || exit 9
-	unzip -u -o 'Server-Files-0.3.2.zip' -d /data
+	curl -Lo 'Server-Files-0.3.3.zip' 'https://edge.forgecdn.net/files/5884/498/Server-Files-0.3.3.zip' || exit 9
+	unzip -u -o 'Server-Files-0.3.3.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
 		cd "${DIR_TEST}"
